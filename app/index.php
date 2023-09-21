8d63cb86d8518670e00885fbfda655b1789b6d90
aarsalannazari <aarsalannazari@gmail.com>
2023-09-21 15:47:20 +0330

    change site to compoents site

---
+++ app/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="keywords"
+    content="iraniandreamhome,smart home,smart,smarthome,cctv,camera,rgb,RGB,bms,bnd,hormozgan,luxury,bandarabbas,lock,qeshm,Electrician,electric,Engineering,Engineer,power electric,learn,learning,smart learning,smart lock,party,decoration,digital,security,YAMAHA,yamaha,video projector,projector,showroom,amplifier,Alexa,alexa,amazon,amazon alexa,google home,voice assistant,قفل هوشمند,دستگیره هوشمند,خانه هوشمند,هوشمند سازی ساختمان,هوشمند سازی,خانه های رویایی ایرانی,هوشمند,خانه مدرن,بندرعباس,هرمزگان,اسمارت هوم,قشم,لاکچری,خاص,لوکس,دکور,دکوراسیون,برق,قفل دیجیتال,مشاوره,دوربین,مداربسته,امنیت,اسمارت,قفل,یاماها,پارتی,تولد,سیستم صوتی,جشن,رقص,سینما,شوروم,سینماروم,باند,آمپلی فایر,دزدگیر,آموزش,آموزش هوشمند,آموزش هوشمند سازی,الکسا,دستیار صوتی,شیر برقی,پرده برقی,پرده هوشمند,نورمخفی,نور مخفی,نور رنگی,استخدام,بازاریاب,بازاریابی,آموزشگاه" />
+  <meta name="description" content="هوشمند سازی ساختمان به صورت کاملا حرفه ای" />
+
+  <meta name="author" content="Amir Arsalan Nazari" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="shortcut icon" href="assets/icons/favicon.ico" type="images/x-icon" />
+  <title>خانه های رویایی ایرانی</title>
+</head>
+
+<body dir="rtl">
+  <!--==================== start noscript section ====================-->
+
+  <noscript>
+    <div style="z-index: 1000"
+      class="d-flex position-fixed bg-white align-items-center justify-content-center vh-100 vw-100">
+      <p class="position-fixed text-danger" style="font-size: 40px">
+        لطفا جاوا اسکریپت مرورگر خود را روشن کنید !<span class="d-block text-center">سپس صفحه را رفرش کنید</span>
+      </p>
+    </div>
+  </noscript>
+
+  <!--==================== end noscript section ====================-->
+
+  <!--==================== start loading section ====================-->
+
+  <div class="bg-loading">
+    <div class="loading-text">در حال بارگزاری...</div>
+    <span class="loading"></span>
+  </div>
+
+  <!--==================== end loading section ====================-->
+
+  <!--==================== start contact section ====================-->
+
+  <?php require "components/social.html" ?>
+
+  <!--==================== end contact section ====================-->
+
+
+  <!--==================== start scripts section ====================-->
+
+  <!--=====Font Awesome Script=====-->
+  <script src="https://kit.fontawesome.com/d7ae55cf3a.js"></script>
+
+  <!--=====Sweet Alert Script=====-->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+  <!--=====jQuery Script=====-->
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+
+  <!--=====Popperjs Script=====-->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+
+  <!--=====Bootstrap Scripts=====-->
+  <!--I can't use both!-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script> -->
+
+  <!--=====Local Scripts=====-->
+  <script src="assets/js/root.js"></script>
+
+  <!--==================== end scripts section ====================-->
+</body>
+
+</html>
