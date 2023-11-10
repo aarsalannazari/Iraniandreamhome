c998dac3d7b3c0538d0451d11d5e23e799be8959
aarsalannazari <aarsalannazari@gmail.com>
2023-11-10 22:06:57 +0330

    edit location of image files and chage __stye__

---
+++ app/build/assets/js/root.js
@@ -0,0 +1,1 @@
+function more_social(){Swal.fire({title:"خطا",text:"این گزینه فعلا فعال نیست !",icon:"error",confirmButtonText:"باشه",confirmButtonColor:"#00c3ff",showCloseButton:!0})}$(document).ready(function(){$(".loading").hide(),$(".bg-loading").hide(),$(".loading-text").hide()});
